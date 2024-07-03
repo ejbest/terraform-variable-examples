dee5fc7c7b83d491722c0d8414f979602948aaa5
EJ Best <ej.best@pm.me>
2024-07-03 08:09:06 -0400

    update for outputs

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 - Very Simple Examples
 - These are just examples and no AWS or Passwords required
 - See how the variables & counting works
+- See how variables are referenced
 - Anything else you would like; let me know
 - Feel free to clone this and use them 
-- Just something simple and love to share  
+- Just something simple and love to share
+- A key word in terraform is "interpolation" 
+- Planning to look at ways to add helpful comments  
--- main.tf
+++ main.tf
@@ -9,8 +9,24 @@ resource "random_pet" "my-pet" {
   length    = var.length
 }
 
-resource "local_file" "pets" {
-  count    = length(var.myfiles)
-  filename = var.myfiles[count.index]
-  content  = "Content for the file"
+# resource "local_file" "pets" {
+#   count    = length(var.myfiles)
+#   filename = var.myfiles[count.index]
+#   content  = "Content for the file"
+# }
+
+ resource "local_file" "pets" {
+  for_each = toset(var.myfiles)
+
+  filename = each.value
+  content  = "This is the content of the file."
+}
+
+output "pets" {
+  value = { for file in local_file.pets : file.filename => {
+    directory_permission = "0777"
+    file_permission      = "0777"
+    filename             = file.filename
+    id                   = file.id
+  } }
 }
